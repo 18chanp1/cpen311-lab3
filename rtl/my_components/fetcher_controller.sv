042f4bbc42210114255be2cf62e64ef1de7ba067
18chanp1 <18chanp1@gmail.com>
2023-05-31 22:43:38 -0700

    speed up down functional

--- rtl/my_components/fetcher_controller.sv
+++ rtl/my_components/fetcher_controller.sv
@@ -16,22 +16,24 @@ module fetcher_controller
     input logic [7:0]   scan_code,
     input logic         kbd_data_ready,
     input logic         rst,
+    input logic         speed_up_event,
+    input logic         speed_down_event,
+    input logic         speed_reset_event,
     output logic        [FREQ_DIV_WIDTH - 1:0] sample_freq_div,
     output logic        pause,
     output logic        forward,
     output logic        fetcher_reset
 );
-    /* Some funny math here to "NOT" the gates so that they have a correct default */
+    /* Some funny math here to "NOT" the gates so that they have a correct default (0 when started) */
     logic [FREQ_DIV_WIDTH + 3 - 1 : 0] state;
-    assign sample_freq_div = (state[32:3] ^ (DEFAULT_FREQ_DIV));
-    assign pause = ~state[2]; //default state 1
-    assign forward = ~state[1]; //default state 1
+    assign sample_freq_div = (state[34:3] + (DEFAULT_FREQ_DIV));
+    assign pause = ~state[2]; 
+    assign forward = ~state[1]; 
     assign fetcher_reset = state[0];
-    
 
     always_ff @(posedge clk) begin
         if (rst) begin
-            state <= {DEFAULT_FREQ_DIV, ~DEFAULT_PAUSE, ~DEFAULT_FORWARD, 1'b0};
+            state <= 35'b0;
         end
         else if (kbd_data_ready)begin
             state[0] <= 1'b0;
@@ -39,10 +41,15 @@ module fetcher_controller
                 D: state[2] <= 1'b0; //pause
                 E: state[2] <= 1'b1; //continue
                 F: state[1] <= 1'b0;  //forward
-                B: state[1] <= 1'b1;
+                B: state[1] <= 1'b1; // backward
                 R: state[0] <= 1'b1; //reset
             endcase
         end 
+        else if(speed_reset_event) state[34:3] <= 32'b0;
+        else if(speed_up_event)state[34:3] <= state[34:3] + 1; 
+        /*fancy magic trick to encode it again*/
+        else if (speed_down_event) state[34:3] <= state[34:3] - 1; 
+        /*fancy magic trick to encode it again*/
         else state[0] <= 1'b0;
     end
 endmodule
